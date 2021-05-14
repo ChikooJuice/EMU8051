2745f0986ad2bde24ebaa448bb600bf8664184d7
Dipesh <65654796+dipesh-does-things@users.noreply.github.com>
2021-05-14 08:14:48 +0530

    Added memory map
    added memory map of 8051, some documentation is left.

---
+++ Memory/Memory.h
@@ -0,0 +1,244 @@
+/** Memory outline of 80c51
+ * 
+ * Total 128 byte RAM
+ * 
+ * R0 - R7 general purpose registers 
+ * 4 General purpose register banks : 0x00 to 0x1F
+ * 
+ * bit addressable Segment : 0x20 - 0x2F
+ * scratch pad area : 0x30 - 0x7F 
+ * 
+ * Special Function Memory Map : 0x80 - 0xFF
+ * 
+ * here, we along with internal data memory or RAM we assume, there
+ * is external DATA memory attached to out microcontroller of size 8k
+ **/
+
+#ifndef MEMORY_H
+#define MEMORY_H
+#include <stdint.h>
+#include <stdlib.h>
+
+#define EXTERNAL_MEMORY 1024 * 8
+
+// SFR Addresses in Memory from 0x80 - 0xFF
+#define 	P0 	0x80
+#define 	SP 	0x81
+#define 	DPL 	0x82
+#define 	DPH 	0x83
+#define 	PCON	0x87
+#define 	TCON 	0x88
+#define 	TMOD 	0x89
+#define 	TL0 	0x8A
+#define 	TL1 	0x8B
+#define 	TH0 	0x8C
+#define 	TH1 	0x8D
+#define 	P1 	0x90
+#define 	SCON 	0x98
+#define 	SBUF 	0x99
+#define 	P2 	0xA0
+#define 	IE 	0xA8
+#define 	P3 	0xB0
+#define 	IP 	0xB8
+#define 	T2CON 	0xC8
+#define 	RCAP2L 	0xCA
+#define 	RCAP2H 	0xCB
+#define 	TL2 	0xCC
+#define		TH2 	0xCD
+#define 	PSW 	0xD0
+#define 	ACC 	0xE0
+#define 	B 	0xF0
+
+
+/** PCON : Program Status Word. Bit Addressable
+ * 
+ * |	P	|	PSW.0	|	Parity Flag			|	
+ * |	_	|	PSW.1	|	User defined			|
+ * |	OV 	|	PSW.2	|	Overflow flag			|
+ * |	RS0	|	PSw.3	|	Register bank selector bit 0	|
+ * |	RS1	| 	PSW.4	|	Register bank selector bit 1	| 
+ * |	F0	| 	PSw.5	| 	Flag 0 available for user	|
+ * |    AC	| 	PSw.6	|	Aux carry flag			|
+ * |	CY	|	PSW.7	|	Carry flag			|
+ *
+ **/
+
+#define		P	(1 << 0)
+//#define 	XX	(1 << 1)
+#define		OV	(1 << 2)
+#define		RS0 	(1 << 3)
+#define		RS1 	(1 << 4)
+#define 	F0	(1 << 5)
+#define 	AC	(1 << 6)
+#define		CY 	(1 << 7)
+
+/** PCON : Power Control Register. Not Bit Addressable
+ * 
+ * |	IDL	|	PCON.0	|	Idle mode bit			|	
+ * |	PD	|	PCON.1	|	Power Down bit			|
+ * |	GF0 	|	PCON.2	|	General purpose bit		|
+ * |	GF1	|	PCON.3	|	General Purpose bit		|
+ * |	_	| 	PCON.4	|	reserved for future use		| 
+ * |	_	| 	PCON.5	| 	reserved for future use		|
+ * |    _	| 	PCON.6	|	reserved for future use		|
+ * |	SMOD	|	PCON.7	|	Double baud Rate bit		|
+ *
+ **/
+
+
+#define		IDL 	(1 << 0)
+#define 	PD	(1 << 1)
+#define		GF0	(1 << 2)
+#define		GF1	(1 << 3)
+//#define	xx 	(1 << 4)
+//#define 	xx	(1 << 5)
+//#define 	xx	(1 << 6)
+#define		SMOD 	(1 << 7)
+
+/** IE : Interrupt Enable. Bit Addressable
+ * 
+ * |	EX0	|	IE.0	|	external interrupt 0		|			|	
+ * |	ET0	|	IE.1	|	Timer 0 overflow interrupt	|			|
+ * |	EX1	|	IE.2	|	External interrupt 0		|
+ * |	ET1	|	IE.3	|	Timer 1 Overflow flag		|
+ * |	ES	| 	IE.4	|	Enable/Disable Serial Port Int	| 
+ * |	__	| 	IE.5	| 	___				|
+ * |    ___	| 	IE.6	|	___				|
+ * |	EA	|	IE.7	|	Disable all interrupts.		|
+ *
+ **/
+
+
+#define		EX0 	(1 << 0)
+#define 	ET0	(1 << 1)
+#define		EX1	(1 << 2)
+#define		ET1 	(1 << 3)
+#define		ES 	(1 << 4)
+#define 	ET2	(1 << 5)
+//#define 	XX	(1 << 6)
+#define		EA 	(1 << 7)
+
+/** IP : Interrupt Priority Register. Bit Addressable
+ * 
+ * |	PX0	|	IP.0	|	Ext Interrupt 0 priority level	|			|	
+ * |	PT0	|	IP.1	|	Timer 0 interrupt priority level|			|
+ * |	PX1 	|	IP.2	|	Ext interrupt 1 priority level	|			|
+ * |	PT1	|	IP.3	|	Timer 1 Interrupt priority level|
+ * |	PS	| 	IP.4	|	Serial port priority level	| 
+ * |	__	| 	IP.5	| 	___				|
+ * |    __	| 	IP.6	|	___				|
+ * |	__	|	IP.7	|	___				|
+ *
+ **/
+
+
+#define		PX0 	(1 << 0)
+#define 	PT0	(1 << 1)
+#define		PX1	(1 << 2)
+#define		PT1 	(1 << 3)
+#define		PS 	(1 << 4)
+//#define 	XX	(1 << 5)
+//#define 	XX	(1 << 6)
+//#define	XX 	(1 << 7)
+
+
+/** TCON : Timer/Counter Control Register. Bit Addressable
+ * 
+ * |	IT0	|	TCON.0	|	Interrupt 0 type control	|			|	
+ * |	IE0	|	TCON.1	|	Ext Int 0 edge flag		|			|
+ * |	IT1 	|	TCON.2	|	Interrupt 1 type control	|			|
+ * |	IE1	|	TCON.3	|	Ext Int 1 edge flag		|
+ * |	TR0	| 	TCON.4	|	Timer 0 run control bit		| 
+ * |	TF0	| 	TCON.5	| 	Timer 0 overflow flag		|
+ * |    TR1	| 	TCON.6	|	Timer 1 run control bit		|
+ * |	TF1	|	TCON.7	|	Timer 1 overflow flag		|
+ **/
+
+#define		IT0 	(1 << 0)
+#define 	IE0	(1 << 1)
+#define		IT1	(1 << 2)
+#define		IE1 	(1 << 3)
+#define		TR0 	(1 << 4)
+#define 	TF0	(1 << 5)
+#define 	TR1	(1 << 6)
+#define		TF1	(1 << 7)
+
+
+/** TMOD : Timer/Counter Mode Control. Not Bit Addressable
+ * |	M0	|	PSW.0/4	|	Mode selector bit 0		|	
+ * |	M1	|	PSW.1/5	|	Mode selector bit 1		|
+ * |	C/T 	|	PSW.2/6	|	Timer or counter selector	|
+ * |	GATE	|	PSw.3/7	|	more info down	|
+ *
+ * 
+ * GATE : when TRx(in TCON) is 1 and GATE = 1, timer counter will run only when
+ * INTx pin is high, it is hardware controlled.
+ * GATE = 0, Timer/counter will run only when when TRx = 1 (software controlled)
+ * 
+ * bits 0 - 3 : Timer 0
+ * bits 4 - 7 : Timer 1
+ **/
+
+
+#define		M0 	(1 << 0)
+#define 	M1	(1 << 1)
+#define		CT	(1 << 2)
+#define		GATE 	(1 << 3)
+#define		M0	(1 << 4)
+#define 	M1	(1 << 5)
+#define 	CT	(1 << 6)
+#define		GATE	(1 << 7)
+
+
+
+// T2CON pending for future
+
+
+/** SCON : Serial Port Control Register. Bit Addressable
+ * 
+ * |	RI	|	SCON.0	|	Receive Interrupt flag				|			|	
+ * |	TI	|	SCON.1	|	Transmit Interrupt Flag				|			|
+ * |	RB8 	|	SCON.2	|	9th data bit received when mode 2 & 3		|			|
+ * |	TB8	|	SCON.3	|	9th data received when mode 2 & 3		|
+ * |	REN	| 	SCON.4	|	Enable/Disable Reception. control by SW		| 
+ * |	SM2	| 	SCON.5	| 	Enables multiprocessor communication in mode 2&3|
+ * |    SM1	| 	SCON.6	|	Serial Port Mode specifier						|
+ * |	SM0	|	SCON.7	|	Serial port mode specifier					|
+ *
+ **/
+
+#define		RI 	(1 << 0)
+#define 	TI	(1 << 1)
+#define		RB8	(1 << 2)
+#define		TB8 	(1 << 3)
+#define		REN 	(1 << 4)
+#define 	SM2	(1 << 5)
+#define 	SM1	(1 << 6)
+#define		SM0	(1 << 7)
+
+
+
+
+
+
+struct GPR {
+	uint8_t R0;
+	uint8_t R1;
+	uint8_t R2;
+	uint8_t R3;
+	uint8_t R4;
+	uint8_t R5;
+	uint8_t R6;
+	uint8_t R7;
+};
+
+
+typedef struct memory {
+	struct GPR REGISTERS[4]; // 4 memory bank registers
+	uint8_t Bit_Addressable[0xF]; // bit addressable area
+	uint8_t ScratchPad[0x4F];
+	uint8_t SFR[0x7F];
+	uint8_t EXT[EXTERNAL_MEMORY];
+};
+
+#endif
---
+++ disassembler/disassembler.c
@@ -0,0 +1,118 @@
+#include "disassembler.h"
+
+struct section DecodeSection (FILE *objfile) {
+
+	struct section CurrSection;
+	CurrSection.len = CurrSection.checksum = CurrSection.address = 0;
+	char tmp[2]; 
+
+	// reading the length of current section
+	fread (len_buffer, sizeof (char), 2, objfile);
+	CurrSection.len = ( 16 * IntfromHex( len_buffer[0]) ) + IntfromHex (len_buffer[1]);
+	
+	//reading the address to load
+	fread (&address_buffer, sizeof(char), 4, objfile);
+	CurrSection.address = (16 * 16 * 16 * IntfromHex ( address_buffer[0] ) ) + \
+				(16 * 16 * IntfromHex ( address_buffer[1] ) ) + \
+				(16 * IntfromHex ( address_buffer[2] ) ) + \
+				IntfromHex ( address_buffer[3] );
+
+	// reading the type of section
+	fread (len_buffer, sizeof (char), 2, objfile);
+	CurrSection.type = ( 16 * IntfromHex ( len_buffer[0])) + \
+				IntfromHex (len_buffer[1]);
+
+	//reading data and converting it to equivalent 
+	// byte and storing them in data of section 
+
+	CurrSection.data = malloc (sizeof (uint8_t) * CurrSection.len);
+	for (int i = 0; i < CurrSection.len; i++) {
+		fread (tmp, sizeof (char), 2, objfile);
+		
+		CurrSection.data[i] = (16 * IntfromHex(tmp[0] )) +  IntfromHex (tmp[1]);
+	}
+
+
+	// parsing the checksum
+	fread ( len_buffer, sizeof (char), 2, objfile);
+	CurrSection.checksum = ( 16 * IntfromHex(len_buffer[0]) ) + IntfromHex (len_buffer[1]);
+
+	// checking the checksum
+	uint8_t checksum_buffer = CurrSection.address + CurrSection.len + CurrSection.type;
+	for (int i = 0; i < CurrSection.len; i++)	checksum_buffer += (CurrSection.data[i]);
+
+	// checksum = summing all bytes subtracted form 256
+	checksum_buffer = 256 - checksum_buffer;
+	checksum_buffer = checksum_buffer < 0? (-1 *checksum_buffer): checksum_buffer;
+
+	if (CurrSection.checksum != checksum_buffer) {
+		printf ("Checksum Error!\n");
+		exit (1);
+	}
+
+	printf ("No checksum error\n");
+
+	return CurrSection;
+}
+
+void printSections (struct section *dissasmbl_list) {
+	struct section *temp = dissasmbl_list;
+	int i = 1;
+	while (temp != NULL) {
+		printf ("\n\nSection %d  address %X \n", i, temp);
+		printf("length : %x \n\
+address : %X \n\
+checksum : %X \n\
+type : %X \n", temp->len, temp->address, temp->checksum, temp->type);
+
+	printf ("data : ");
+	for (int j = 0; j < temp->len; j++)	printf("%X ",temp->data[j]);
+	printf("\nnext : %x \n", temp->next);
+	temp = temp->next;
+	i++;
+	}
+}
+
+
+	
+
+int IntfromHex (char tmp) {
+	switch (tmp)
+	{
+	case '1':
+		return 1;
+	case '2':
+		return 2;
+	case '3':
+		return 3;
+	case '4':
+		return 4;
+	case '5':
+		return 5;
+	case '6':
+		return 6;
+	case '7':
+		return 7;
+	case '8':
+		return 8;
+	case '9':
+		return 9;
+	case 'A':
+		return 10;
+	case 'B':
+		return 11;
+	case 'C':
+		return 12;
+	case 'D':
+		return 13;
+	case 'E':
+		return 14;
+	case 'F':
+		return 15;
+	case '0':
+		return 0;
+	default:
+		return 0;
+		break;
+	}
+}
